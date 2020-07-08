f8d777a59a3153b2a3e5322734cecece40edf5c8
sleepanda4523 <63851752+sleepanda4523@users.noreply.github.com>
2020-07-08 18:44:58 +0900

    COMMIT

--- NEWINFO/함수포인터-사칙연산/sample.c
+++ NEWINFO/함수포인터-사칙연산/sample.c
@@ -5,30 +5,30 @@ typedef struct CAL {
 	char c;
 	double b;
 }cal;
-void add(double, double);
-void sub(double, double);
-void mul(double, double);
-void div(double, double);
+double add(double, double);
+double sub(double, double);
+double mul(double, double);
+double div(double, double);
 
 int main()
 {
 	cal ndata;
-	void(*fp[4])(double,double) = { add,sub,mul,div };
+	double(*fp[4])(double,double) = { add,sub,mul,div };
 	printf("사칙연산 수식을 입력해주세요 >> ");
 	scanf("%d %c %d", &ndata.a, &ndata.c, &ndata.b);
 	switch (ndata.c)
 	{
 	case '+':
-		fp[0](ndata.a, ndata.b);
+		printf("%.4lf\n",fp[0](ndata.a, ndata.b));
 		break;
 	case '-':
-		fp[1](ndata.a, ndata.b);
+		printf("%.4lf\n", fp[1](ndata.a, ndata.b));
 		break;
 	case '*':
-		fp[2](ndata.a, ndata.b);
+		printf("%.4lf\n", fp[2](ndata.a, ndata.b));
 		break;
 	case '/':
-		fp[3](ndata.a, ndata.b);
+		printf("%.4lf\n", fp[3](ndata.a, ndata.b));
 		break;
 	default:
 		printf("사칙연산이 아닙니다..!\n");
@@ -36,24 +36,24 @@ int main()
 	}
 	return 0;
 }
-void add(double a, double b)
+double add(double a, double b)
 {
-	printf("%.4lf\n", a + b);
+	return a + b;
 }
-void sub(double a, double b)
+double sub(double a, double b)
 {
-	printf("%.4lf\n", a - b);
+	return a - b;
 }
-void mul(double a, double b)
+double mul(double a, double b)
 {
-	printf("%.4lf\n", a * b);
+	return a * b;
 }
-void div(double a, double b)
+double div(double a, double b)
 {
-	if (b > a || b == 0) {
+	if (b == 0) {
 		printf("계산할 수 없습니다!\n");
 	}
 	else {
-		printf("%.4lf\n", a / b);
+		return a / b;
 	}
 }
